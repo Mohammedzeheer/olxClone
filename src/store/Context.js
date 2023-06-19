584752b207cae00874a92078934b6c81c0a8f699
Mohammedzeheer <zeheerzak@gmail.com>
2023-06-19 05:42:23 +0530

    login and username worked

---
+++ src/store/Context.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+export const FirebaseContext = createContext(null)
+
+export const AuthContext = createContext(null)
+
+
+export default function Context({children}){
+    const [user,setUser]=useState('')
+
+    return (
+        <AuthContext.Provider value={{user,setUser}}>
+             {children}
+        </AuthContext.Provider>
+    )
+}
